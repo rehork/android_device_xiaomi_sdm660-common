54f849a21a7a7902b61da996550540eef44ca4fb
firebird11 <hbgassel@gmail.com>
2021-03-14 16:18:32 +0530

    sdm660-common: Build Mock Powerstats HAL
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL

--- sdm660.mk
+++ sdm660.mk
@@ -426,7 +426,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    vendor.qti.hardware.perf@2.1.vendor
+    vendor.qti.hardware.perf@2.1.vendor \
+    android.hardware.power.stats@1.0-service.mock
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
