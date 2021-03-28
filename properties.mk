f0bb55728618a63f97db04a0407541ae03486ebc
MrYacha <yacha@orangefox.tech>
2021-03-28 15:16:40 -0500

    Enable powersave modem functionality https://github.com/sonyxperiadev/device-sony-common/blob/da895ebf26f84df532ae43fb3e0ca1fe57db91ef/common-prop.mk#L52
    Change-Id: I4ff27358d86ec0f18c6943b6ba5aa596ac1c7198

--- properties.mk
+++ properties.mk
@@ -193,7 +193,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.rat_on=combine \
     persist.vendor.radio.sib16_support=1 \
     ro.telephony.default_network=22,22 \
-    ro.vendor.use_data_netmgrd=true
+    ro.vendor.use_data_netmgrd=true \
+    persist.vendor.radio.add_power_save=1
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     DEVICE_PROVISIONED=1 \
