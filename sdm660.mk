fd5ebc4d5046414501f305a3d5bfc5d25f8103dc
rehork <67607997+rehork@users.noreply.github.com>
2023-06-20 17:35:06 +0000

    SDM660-common: Use legacy Wi-Fi service
    HalDevMgr: getAllChipInfo: called but mWifi is null!?

--- sdm660.mk
+++ sdm660.mk
@@ -456,6 +456,7 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     hostapd \
     libwifi-hal-qcom \
     WifiOverlay \
